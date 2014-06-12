60328598af8285525b45796cfee21c991f7320a6
Andy Hawkes <andrew.hawkes@rga.com>
2014-06-12 11:02:38 +0100

    Updated README, added script file

---
+++ PathsToImageMap.jsx
@@ -0,0 +1,167 @@
+/*****************************************************************
+*
+* PathsToImageMap 1.0 - by Andy Hawkes - http://www.andyhawkes.co.uk/
+*
+* v 1.0 - 2010.10.14
+*
+* Licensed under the Creative Commons Attribution 2.5 License - http://creativecommons.org/licenses/by/2.5/
+*
+*****************************************************************/
+
+
+/*
+* -------------------------------------------------------------
+* Array search helper function
+* -------------------------------------------------------------
+*/
+
+function checkArrayForValue(haystack, needle) {
+	haystack.sort();
+	for (var i = 0; i < haystack.length; i++) {
+		if (haystack[i] == needle) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+/*
+* -------------------------------------------------------------
+* PathsToImageMap Core Function
+* -------------------------------------------------------------
+*/
+
+function doPathExport(el, fileOutput, filePath) 
+{
+
+	// Get the paths within the current document
+	var paths = el.pathItems;
+
+	// Start the image map output
+	fileOutput.writeln('<map name="imageMap">');
+
+	// Loop through all paths
+	for (var pathIndex = paths.length; pathIndex > 0; pathIndex--)
+	{
+		// Get the point coordinates for the current path
+		var currentPath = paths[pathIndex-1];
+		// Make an array to hold points
+		var points = [];
+		// Make arrays to hold point x and y coordintes so we can check if the current path is rectangular
+		var xvals = [];
+		var yvals = [];
+		// Loop through the sub-path items
+		for (var u = 0; u < currentPath.subPathItems.length; u++){
+			for (var t = 0; t < currentPath.subPathItems[u].pathPoints.length; t++){
+				var point = currentPath.subPathItems[u].pathPoints[t];
+				// Get the anchor coordinates for the point
+				var myAnchor = point.anchor;
+				// Round the values out nicely
+				var myx = Math.round(myAnchor[0]);
+				var myy = Math.round(myAnchor[1]);
+				// Add the coordinates to our arrays
+				points.push([myx,myy]);
+				if( checkArrayForValue(xvals,myx) == false ) { xvals.push(myx); }
+				if( checkArrayForValue(yvals,myy) == false ) { yvals.push(myy); }
+			}
+		}
+		// Output the path coordinate data
+		fileOutput.writeln('');
+		if( points.length == 4 && xvals.length == 2 && yvals.length == 2 ) {
+			var rectPoints = [points[0],points[2]];
+			fileOutput.writeln('<area shape="rect" coords="' + rectPoints + '" href="***'+ currentPath.name + '***.html" alt="' + currentPath.name +'" title="' + currentPath.name +'"/>');
+		} else {
+			fileOutput.writeln('<area shape="poly" coords="' + points + '" href="***'+ currentPath.name + '***.html" alt="' + currentPath.name +'" title="' + currentPath.name +'"/>');
+		}
+	}
+
+	// Finish the image map output
+	fileOutput.writeln('');
+	fileOutput.writeln('</map>');
+}
+
+
+/*
+* -------------------------------------------------------------
+*  PathsToImageMap initialisation function
+* -------------------------------------------------------------
+*/
+
+
+function initPathsToImageMap() {
+
+	// Establish the correct linefeed type
+	if ($.os.search(/windows/i) != -1) {
+		fileLineFeed = "windows";
+	} else {
+		fileLineFeed = "macintosh";
+	}
+
+	// Do we have a document open?
+	if (app.documents.length === 0) {
+		alert("Please open a file", "PathsToImageMap Error", true);
+		return;
+	}
+	
+	// If we have more than one document open...
+	if (app.documents.length > 1) {
+	
+		var runMultiple = confirm("PathsToImageMap has detected multiple open files.\nDo you wish to run PathsToImageMap on all open files?", true, "PathsToImageMap");
+				
+		if (runMultiple === true) {
+			docs = app.documents;
+		} else {
+			docs = [app.activeDocument];
+		}
+	
+	// Or only one document open...
+	} else {
+		runMultiple = false;
+		docs = [app.activeDocument];
+	}
+	
+	// Loop through all documents
+	for (var i = 0; i < docs.length; i++)
+	{
+	
+		// Auto set filePath and fileName
+		filePath = Folder.myDocuments + '/PathsToImageMap-' + docs[i].name + '.txt';
+
+		// create outfile
+		var fileOutput = new File(filePath);
+
+		// set linefeed
+		fileOutput.linefeed = fileLineFeed;
+
+		// open for write
+		fileOutput.open("w", "TEXT", "????");
+
+		// Set active document
+		app.activeDocument = docs[i];
+		
+		// call to the core with the current document
+		doPathExport(app.activeDocument, fileOutput, '/');
+
+		// close the file
+		fileOutput.close();
+	
+	}
+	
+	// Finish up by displaying a notice (multiple) or opening the output file (single)
+	if (runMultiple === true) {
+		alert("PathsToImageMap has parsed " + documents.length + " files;\nFiles were saved in your documents folder", "PathsToImageMap");
+	} else {
+		fileOutput.execute();
+	}
+
+}
+
+
+/*
+* -------------------------------------------------------------
+*  PathsToImageMap instantiation
+* -------------------------------------------------------------
+*/
+
+initPathsToImageMap();
--- README.md
+++ README.md
@@ -1,4 +1,83 @@
 ps-paths-to-imagemap
 ====================
 
-Photoshop imagemap generation script
+A Photoshop imagemap generation script
+
+I was once trying to find an easy way of creating a complex image map that didn’t include Dreamweaver, clunky online tools, or abject frustration... and I failed.
+
+Instead of just moaning about it on Twitter (like I would usually do), I did something about it - and the result is a handy little Photoshop script.
+
+## Here’s what you need to do:
+
+1. Download the PathsToImageMap.jsx script
+2. Open the image that you want to map in Photoshop
+3. Define each area as a path
+4. Run the PathsToImageMap script
+5. That’s it. The script will present you with a text file containing the map HTML.
+
+All you need to do then is drop it in to your HTML page with the appropriate image (I’m not going to go in to detail on that here - read up on it if you don’t already know how).
+
+## Need a little more detail?
+
+OK...
+
+The script will work on a single photoshop file, or all open files if you want it to.
+
+It will generate rectangular and polygonal areas, but not circular ones (see below).
+
+It will use the name you have given to each path to populate the href, alt and title attributes of the resulting area element.
+
+You will need to tweak the resulting output before using it in anger, but it makes defining complex polygonal areas a lot easier.
+
+The polygons will be defined by the path points, so you’ll need to make sure that you convert points from curves to corners to get the best results.
+
+## So why is this useful?
+
+OK... imagine you’ve got a map of your country of residence, visually divided in to nice blocks of colour for each state/county/region/whatever, and let’s also imagine that those regions weren’t defined by a cartographer with a ruler (so they’ve got wiggly edges).
+
+1. Select the “magic wand” tool
+2. Use it to select a specific region
+3. Click on the “Make work path from selection” button in the paths palette
+4. Give the work path a name
+5. Repeat the above until you’ve got all of your areas defined as named paths
+6. Run the script
+
+Isn’t that easier than clicking a ton of polygonal reference points?
+
+## Some of the polygons are a bit clunky though...
+
+So neaten the paths up in Photoshop before you run the script.
+
+What do you want from me? Blood? ;-)
+
+As mentioned above, the polygons will be defined by the path points so you should convert points from curves to corners to get the best results - it’s a bit more work, but it’s still easier than defining the entire polygon point by point with a million mouse clicks…
+
+## Here’s what it won’t do:
+
+It doesn’t do circular areas (at the moment) - if you define a circular selection and convert it to a path you’ll end up with a polygonal area until I can work out some more complicated Photoshop scripting voodoo.
+
+It doesn’t create the <img> element or the associated usemap attribute
+
+## Here’s the kind of code it will output:
+
+    <map name="imageMap"> <area shape="poly" coords="233,36,244,37,241,53,214,79,207,80,219,82,219,83,216,88,202,92,202,94,203,94,213,90,202,99,202,100,231,88,278,94,280,102,257,151,234,163,249,158,253,166,253,167,217,169,218,164,202,160,207,156,194,154,193,153,199,129,194,123,169,123,164,115,169,110,172,113,163,108,167,102,160,104,159,97,161,93,170,95,162,86,165,86,167,85,162,77,165,77,168,83,169,81,167,75,168,75,179,78,176,75,184,80,173,65,179,65,179,64,176,57,188,58,183,53,182,50,186,50,184,43,190,37,197,41,194,46,199,40,204,41,203,45,235,40,235,39,170,102,168,106,172,102" href="***Example polygonal area***.html" alt="Example polygonal area" title="Example polygonal area"/> <area shape="rect" coords="322,40,522,40,522,224,322,224" href="***Example rectangular area***.html" alt="Example rectangular area" title="Example rectangular area"/> </map>
+
+## OK. That’s pretty handy. How to I install that script so Photoshop always has it to hand in the “Scripts” menu?
+
+After downloading the script you will need to copy it into your Scripts folder.
+
+On a PC, the path would be something like:
+
+    C:\Program Files\Adobe\Photoshop (CS or CS2 or whatever version you've got)\Presets\Scripts\
+
+On a MAC the path would be something like:
+
+    Applications> Photoshop (CS or CS2 or whatever version you've got)> Presets> Scripts.
+
+After copying a script to this folder you’ll need to Quit and then Restart Photoshop before the script appears in the File > Scripts menu.
+
+## Anything else?…
+
+Nope, that’s it. I hope you find the script useful.
+
+It’s licensed under the Creative Commons Attribution 2.5 License so you can use it, expand on it, and generally marvel at it (as long as you publicly acknowledge my brilliance)
